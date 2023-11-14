2c9c5e747e851f9b1274b2cf21c2e046ca1577b8
Влад Наполов <85844885+1Desine@users.noreply.github.com>
2023-11-14 04:12:23 +0300

    Reconciliation work but doesn't assing velocity
    1 server and 2 clients: client 2 can not see client 1 move

--- Assets/Sripts/TrooperController.cs
+++ Assets/Sripts/TrooperController.cs
@@ -1,10 +1,8 @@
-using Newtonsoft.Json.Bson;
 using System;
 using System.Collections.Generic;
 using Unity.Multiplayer.Samples.Utilities.ClientAuthority;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.UIElements;
 using Utilities;
 
 
@@ -52,7 +50,7 @@ public class TrooperController : NetworkBehaviour {
 
     // Netcode general
     NetworkTimer networkTimer;
-    const float serverTickRate = 60f;
+    const float serverTickRate = 20f; // set to 60
     const int bufferSize = 1024;
 
     // Network client specific
@@ -67,13 +65,10 @@ public class TrooperController : NetworkBehaviour {
     [Header("Netcode")]
     [SerializeField] float reconceliationThreshold = 10f;
     [SerializeField] float reconceliationCooldownTime = 1f;
-    [SerializeField] float extrapolationLimit = 0.5f; // 0.5f = 500 milliseconds
-    [SerializeField] float extrapolationMultiplayer = 1.2f;
     [SerializeField] Transform serverSphere;
     [SerializeField] Transform clientSphere;
 
-    StatePayload extrapolationState;
-    CountdownTimer extrapolationTimer;
+    StatePayload intrapolationState;
 
 
     CountdownTimer reconceliationTimer;
@@ -91,117 +86,66 @@ public class TrooperController : NetworkBehaviour {
         serverInputQueue = new Queue<InputPayload>();
 
         reconceliationTimer = new CountdownTimer(reconceliationCooldownTime);
-        extrapolationTimer = new CountdownTimer(extrapolationLimit);
-        extrapolationTimer.OnTimerStart += () => {
-            reconceliationTimer.Stop();
-            SwitchAuthorityMode(AuthorityMode.Server);
-        };
-        extrapolationTimer.OnTimerStop += () => {
-            extrapolationState = default;
-            SwitchAuthorityMode(AuthorityMode.Client);
-        };
     }
     private void SwitchAuthorityMode(AuthorityMode mode) {
         clientNetworkTransform.authorityMode = mode;
-        bool shouldSync = mode == AuthorityMode.Client;
-        clientNetworkTransform.SyncPositionX = shouldSync;
-        clientNetworkTransform.SyncPositionY = shouldSync;
-        clientNetworkTransform.SyncPositionZ = shouldSync;
     }
     private void Start() {
-        //body.isKinematic = true;
         serverSphere.parent = null;
+        clientSphere.parent = null;
     }
     private void Update() {
-        //body.isKinematic = false;
-        //Debug.Log("extrapolationTimer.IsRunning: "+ extrapolationTimer.IsRunning);
-
         networkTimer.Update(Time.deltaTime);
         reconceliationTimer.Tick(Time.deltaTime);
-        extrapolationTimer.Tick(Time.deltaTime);
 
 
-        if (IsOwner == false) return;
-        if (Input.GetKeyDown(KeyCode.Q)) transform.position -= transform.forward * 10;
-        if (Input.GetKeyDown(KeyCode.E)) transform.position += transform.forward * 10;
-
-
-        // Run on Update of FixedUpdate, or both - depends on the game, consider exposing an option to the game
-        Extrapolate();
+        if (IsOwner) {
+            if (Input.GetKeyDown(KeyCode.Q)) transform.position -= transform.forward * 10;
+            if (Input.GetKeyDown(KeyCode.E)) transform.position += transform.forward * 10;
+        }
     }
     private void FixedUpdate() {
         while (networkTimer.ShouldTick()) {
             HandleClientTick();
             HandleServerTick();
         }
-        Extrapolate();
     }
+    private void HandleIntrapolation() {
+        if (IsOwner || IsServer) return;
 
-    private void Extrapolate() {
-        if (IsServer && extrapolationTimer.IsRunning) {
-            //transform.position += extrapolationState.position.With(y: 0);
-            body.velocity = extrapolationState.velocity.With(y: 0);
-        }
+        // intrapolation for clients
     }
     private void HandleServerTick() {
         if (!IsServer) return;
 
         var bufferIndex = -1;
-        InputPayload inputPayLoad = default;
+        SwitchAuthorityMode(AuthorityMode.Server);
+        body.isKinematic = false; //
         while (serverInputQueue.Count > 0) {
-            inputPayLoad = serverInputQueue.Dequeue();
+            InputPayload inputPayLoad = serverInputQueue.Dequeue();
 
             bufferIndex = inputPayLoad.tick % bufferSize;
-
             StatePayload statePayload = ProcessMovement(inputPayLoad);
+
             serverStateBuffer.Add(statePayload, bufferIndex);
         }
+        SwitchAuthorityMode(AuthorityMode.Client);
 
         if (bufferIndex == -1) return;
         SendToClientRpc(serverStateBuffer.Get(bufferIndex));
-        HandleExtrapolation(serverStateBuffer.Get(bufferIndex), CalculateLatencyInMilliseconds(inputPayLoad));
+        serverSphere.position = serverStateBuffer.Get(bufferIndex).position; // DEBUG SPHERE
     }
     private static float CalculateLatencyInMilliseconds(InputPayload inputPayload) {
         return (DateTime.Now - inputPayload.timestamp).Milliseconds / 1000f;
     }
-    private bool ShoutldExtrapolate(float latency) => latency < extrapolationLimit && latency > Time.fixedDeltaTime;
-    private void HandleExtrapolation(StatePayload latest, float latency) {
-        if (ShoutldExtrapolate(latency)) {
-            if (extrapolationState.position != default) latest = extrapolationState;
-
-            var positionAdjustent = latest.velocity * (1 + latency * extrapolationMultiplayer);
-            extrapolationState.position = positionAdjustent;
-            extrapolationState.velocity = latest.velocity;
-
-            extrapolationTimer.Start();
-        }
-        else {
-            extrapolationTimer.Stop();
-            // Reconcile if defired
-        }
-    }
-
     [ClientRpc]
     private void SendToClientRpc(StatePayload statePayload) {
-        if (IsServer || IsHost)
-            serverSphere.position = lastServerState.position; // DEBUG SPHERE
-
         if (IsOwner == false) return;
         lastServerState = statePayload;
     }
-    //private StatePayload SimulateMovement(InputPayLoad inputPayLoad) {
-    //    Physics.simulationMode = SimulationMode.Script;
-    //    Move(inputPayLoad.inputVector);
-    //    Physics.Simulate(Time.fixedDeltaTime);
-    //    Physics.simulationMode = SimulationMode.FixedUpdate;
-    //
-    //    return new StatePayload() {
-    //        tick = inputPayLoad.tick,
-    //        position = transform.position,
-    //    };
-    //}
     private void HandleClientTick() {
         if (!IsClient || !IsOwner) return;
+        HandleServerReconciliation();
 
         var currentTick = networkTimer.currentTick;
         var bufferIndex = currentTick % bufferSize;
@@ -220,9 +164,18 @@ public class TrooperController : NetworkBehaviour {
 
         StatePayload statePayload = ProcessMovement(inputPayLoad);
         clientStateBuffer.Add(statePayload, bufferIndex);
-
-        HandleServerReconciliation();
     }
+    private StatePayload ProcessMovement(InputPayload input) {        
+        Move(input.inputVector);
+
+        return new StatePayload() {
+            tick = input.tick,
+            networkObjectId = input.networkObjectId,
+            position = transform.position,
+            velocity = body.velocity,
+        };
+    }
+
     [ServerRpc]
     private void SendToServerRpc(InputPayload input) {
         clientSphere.position = input.position; // DEBUG SPHERE
@@ -235,68 +188,54 @@ public class TrooperController : NetworkBehaviour {
                                             || !lastProcessedState.Equals(lastServerState);
 
         return isNewServerState && isLastStateUndefinedOrDifferent
-            && !reconceliationTimer.IsRunning
-            && !extrapolationTimer.IsRunning;
+            && !reconceliationTimer.IsRunning;
     }
     private void HandleServerReconciliation() {
         if (!ShouldReconcile()) return;
 
-        float positionErrof;
-        int bufferIndex;
-
-        bufferIndex = lastServerState.tick % bufferSize;
+        int bufferIndex = lastServerState.tick % bufferSize;
         if (bufferIndex - 1 < 0) return; // Not enough informaion to reconcile
 
-        StatePayload rewindState = IsHost ? serverStateBuffer.Get(bufferIndex - 1) : lastServerState; // Host RPCs execute immedietelt so we can use the last server state
-        StatePayload clientState = IsHost ? clientStateBuffer.Get(bufferIndex - 1) : clientStateBuffer.Get(bufferIndex);
-        positionErrof = Vector3.Distance(rewindState.position, clientState.position);
-        //positionErrof = Vector3.Distance(lastServerState.position, clientStateBuffer.Get(bufferIndex).position);
+        float positionErrof = Vector3.Distance(lastServerState.position, clientStateBuffer.Get(bufferIndex).position);
+
+        Debug.Log("positionErrof: " + positionErrof);
 
         if (positionErrof > reconceliationThreshold) {
             Debug.Log("Had to Reconcile, diviation is: " + positionErrof);
-            //Debug.Break();
-            ReconcileState(rewindState);
+            ReconcileState();
             reconceliationTimer.Start();
         }
 
+        //intrapolationState = lastProcessedState; // intrapolation for clients
         lastProcessedState = lastServerState;
     }
-    private void ReconcileState(StatePayload rewindState) {
-        transform.position = rewindState.position;
-        body.velocity = Vector3.zero;//rewindState.velocity;
+    private void ReconcileState() {
+        transform.position = lastServerState.position;
+        body.velocity = lastServerState.velocity; // rewindState.velocity is v3.zero for some reason
+        //Debug.Log("rewindState vel: " + rewindState.velocity);
 
-        if (!rewindState.Equals(lastServerState)) return;
+        clientStateBuffer.Add(lastServerState, lastServerState.tick);
 
-        clientStateBuffer.Add(rewindState, rewindState.tick);
-
-        // Replay all inputs front the rewind state to the current state
-        int tickToReplay = lastServerState.tick;
+        // Replay all inputs from the rewind state to the current state
+        int tickToReplay = lastServerState.tick + 1;
 
         while (tickToReplay < networkTimer.currentTick) {
             int bufferIndex = tickToReplay % bufferSize;
-            StatePayload statePayload = ProcessMovement(clientInputBuffer.Get(bufferIndex));
-            clientStateBuffer.Add(statePayload, bufferIndex);
+            StatePayload recalculatedClientPayload = ProcessMovement(clientInputBuffer.Get(bufferIndex));
+            clientStateBuffer.Add(recalculatedClientPayload, bufferIndex);
             tickToReplay++;
         }
     }
-    private StatePayload ProcessMovement(InputPayload input) {
-        Move(input.inputVector);
-
-        return new StatePayload() {
-            tick = input.tick,
-            networkObjectId = input.networkObjectId,
-            position = transform.position,
-            velocity = body.velocity,
-        };
-    }
-
 
     private void Move(Vector2 moveInput) {
         Vector3 moveDirection = transform.forward * moveInput.y + transform.right * moveInput.x;
 
-        float moveSpeed = 200f;
+        float moveSpeed = 5f;
         //transform.position += moveDirection * moveSpeed * networkTimer.minTimeBetweenTicks;
-        body.velocity = moveDirection * moveSpeed * networkTimer.minTimeBetweenTicks;
+        body.velocity += moveDirection * moveSpeed * networkTimer.minTimeBetweenTicks;
+
+
+        Debug.Log("Move, vel = " + body.velocity);
     }
 
 
--- Assets/Sripts/UI/LobbyUI.cs
+++ Assets/Sripts/UI/LobbyUI.cs
@@ -3,11 +3,16 @@ using UnityEngine.UI;
 
 public class LobbyUI : MonoBehaviour {
 
+    [SerializeField] Button StartServerButton;
     [SerializeField] Button StartHostButton;
     [SerializeField] Button StartClientButton;
 
 
     private void Awake() {
+        StartServerButton.onClick.AddListener(() => {
+            MyNetworkManager.Singleton.StartServer();
+            Hide();
+        });
         StartHostButton.onClick.AddListener(() => {
             MyNetworkManager.Singleton.StartHost();
             Hide();
